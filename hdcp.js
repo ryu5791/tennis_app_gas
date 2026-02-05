9234f444b7477b879f836f3747dc1daff232df42
masahiro ichiryu <ryu5791@yahoo.co.jp>
2026-02-06 07:05:39 +0900

    v3.00

---
+++ hdcp.js
@@ -0,0 +1,13 @@
+/**
+ * HDCP計算処理
+ * v3.01 - 新機能追加
+ */
+
+/**
+ * HDCP計算ボタン押下時の処理
+ * HDCPシートのボタンに割り当てる関数
+ */
+function calculateHDCP() {
+  const ui = SpreadsheetApp.getUi();
+  ui.alert('HDCP計算テスト');
+}
